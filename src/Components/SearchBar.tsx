b962a13d3faa8e351779db9302f552b5dc4d93b3
Matt-Hulme <Mattwhulme@gmail.com>
2023-12-06 22:47:08 -0700

    Background-image changed

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -2,7 +2,7 @@ export function SearchBar() {
   return (
     <>
       <input
-        className="w-30rem h-1.5rem rounded-xl border-.25 p-l-.5rem"
+        className="w-22rem h-1.6rem rounded-3xl border-.25 p-l-.5rem"
         placeholder="Search for a country"
       ></input>
     </>
