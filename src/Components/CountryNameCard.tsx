5e2f129ceea60ebe44ab6176c9a0cbb3944cca24
Matt-Hulme <Mattwhulme@gmail.com>
2023-12-10 01:47:27 -0700

    Removed an unused UseEffect

--- src/Components/CountryNameCard.tsx
+++ src/Components/CountryNameCard.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { CountryDetailsModal } from "./CountryDetailsModal";
 import { Country } from "../Queries/useGetCountriesListQuery";
 import { useGetCountryDetailsQuery } from "../Queries/useGetCountryDetailsQuery";
